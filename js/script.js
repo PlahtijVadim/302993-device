848729de2601527ea6f2a537435f1beef505daef
demidemid <iamdemid@gmail.com>
2017-02-21 21:32:55 +0300

    fix JS write us

--- js/script.js
+++ js/script.js
@@ -1,63 +1,65 @@
-var writeusOpen = document.querySelector(".js-open-popup");
+(function() {
+  var writeusOpen = document.querySelector(".js-open-popup");
 
-      // Переменный pop-up формы обратной связи
-      var writeusPopup = document.querySelector(".write-us-popup");
-      var writeusClose = writeusPopup.querySelector(".cross-button");
+  // Переменный pop-up формы обратной связи
+  var writeusPopup = document.querySelector(".write-us-popup");
+  var writeusClose = writeusPopup.querySelector(".cross-button");
 
-      var form = writeusPopup.querySelector("form");
-      var name = writeusPopup.querySelector("input[name=name]");
-      var message = writeusPopup.querySelector("textarea");
+  var form = writeusPopup.querySelector("form");
+  var name = writeusPopup.querySelector("input[name=name]");
+  var message = writeusPopup.querySelector("textarea");
 
-      // Переменные pop-up карты
-      var mapOpen = document.querySelector(".js-open-map");
+  // Переменные pop-up карты
+  var mapOpen = document.querySelector(".js-open-map");
 
-      var mapPopup = document.querySelector(".map-google");
-      var mapClose = mapPopup.querySelector(".cross-button");
+  var mapPopup = document.querySelector(".map-google");
+  var mapClose = mapPopup.querySelector(".cross-button");
 
-      // Код для формы обратной связи
-      writeusOpen.addEventListener("click", function(event) {
-        event.preventDefault();
-        writeusPopup.classList.add("modal-content-show");
-      });
+  // Код для формы обратной связи
+  writeusOpen.addEventListener("click", function(event) {
+    event.preventDefault();
+    writeusPopup.classList.add("modal-content-show");
+  });
 
-      writeusClose.addEventListener("click", function(event) {
-        event.preventDefault();
+  writeusClose.addEventListener("click", function(event) {
+    event.preventDefault();
+    writeusPopup.classList.remove("modal-content-show");
+    writeusPopup.classList.remove("modal-error");
+  });
+
+  form.addEventListener("submit", function(event) {
+    if (!name.value || !message.value) {
+      event.preventDefault();
+      writeusPopup.classList.remove("modal-error");
+      writeusPopup.offsetWidth;
+      writeusPopup.classList.add("modal-error");
+    }
+  });
+
+  window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+      if (writeusPopup.classList.contains("modal-content-show")) {
         writeusPopup.classList.remove("modal-content-show");
-        writeusPopup.classList.remove("modal-error");
-      });
+      }
+    }
+  });
 
-      form.addEventListener("submit", function(event) {
-        if (!name.value || !message.value) {
-          event.preventDefault();
-          writeusPopup.classList.add("modal-error");
-          writeusPopup.offsetWidth;
-          // writeusPopup.classList.remove("modal-error");
-        }
-      });
+  // Код для карты
+  mapOpen.addEventListener("click", function(event) {
+    event.preventDefault();
+    mapPopup.classList.add("modal-content-show");
+  });
 
-      window.addEventListener("keydown", function(event) {
-        if (event.keyCode === 27) {
-          if (writeusPopup.classList.contains("modal-content-show")) {
-            writeusPopup.classList.remove("modal-content-show");
-          }
-        }
-      });
+  mapClose.addEventListener("click", function(event) {
+    event.preventDefault();
+    mapPopup.classList.remove("modal-content-show");
+  });
 
-      // Код для карты
-      mapOpen.addEventListener("click", function(event) {
-        event.preventDefault();
-        mapPopup.classList.add("modal-content-show");
-      });
-
-      mapClose.addEventListener("click", function(event) {
-        event.preventDefault();
+  window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+      if (mapPopup.classList.contains("modal-content-show")) {
         mapPopup.classList.remove("modal-content-show");
-      });
-
-      window.addEventListener("keydown", function(event) {
-        if (event.keyCode === 27) {
-          if (mapPopup.classList.contains("modal-content-show")) {
-            mapPopup.classList.remove("modal-content-show");
-          }
-        }
-      });
+      }
+    }
+  });
+})();
